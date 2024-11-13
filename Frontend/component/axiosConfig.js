d45450455e1264e89318f27c17fc1b8f01a669e9
Venkatesh TamilSelvam <tamilofficial2@gmail.com>
2024-11-13 12:41:12 +0530

    Update axiosConfig.js

--- Frontend/component/axiosConfig.js
+++ Frontend/component/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://1cde-103-130-91-93.ngrok-free.app',
+  baseURL: 'https://951a-103-130-91-93.ngrok-free.app',
   timeout: 5000, // Set a timeout for the request
   headers: {
     'Content-Type': 'application/json',
